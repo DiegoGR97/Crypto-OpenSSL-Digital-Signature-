90b9e2864c4520d32dbd2957d7bc624b3ef27cb1
Diego José González Roca <diegogr_97@hotmail.com>
2020-07-23 14:35:04 -0600

    New child certificate now being created with serial number according to the directories already existing.

--- newChildCertificate.js
+++ newChildCertificate.js
@@ -2,132 +2,208 @@ const fs = require("fs");
 var forge = require("node-forge");
 var pki = forge.pki;
 
-// generate a keypair or use one you have already
-var keys = pki.rsa.generateKeyPair(2048);
+let rootCertificateCertAndKeyPairExist = true;
+let tempDir = "./Certificates";
+if (!fs.existsSync(tempDir)) {
+  console.log("No existe el folder ./Certificates");
+  rootCertificateCertAndKeyPairExist = false;
+  //fs.mkdirSync(tempDir);
+}
 
-// create a new certificate
-var cert = pki.createCertificate();
+let rootPrivateKey = "";
+let rootCertificate = "";
+if (!fs.existsSync("./Certificates/rootCertificate/rootCertificate.pem")) {
+  console.log("No existe ./Certificates/rootCertificate/rootCertificate.pem.");
+  rootCertificateCertAndKeyPairExist = false;
+} else {
+  const rootCertificatePEM = fs.readFileSync(
+    "./Certificates/rootCertificate/rootCertificate.pem",
+    "utf-8"
+  );
+  console.log("rootCertificatePEM: \n", rootCertificatePEM, "\n");
+  rootCertificate = pki.certificateFromPem(rootCertificatePEM);
+}
 
-// fill the required fields
-cert.publicKey = keys.publicKey;
-let pemPublicKey = pki.publicKeyToPem(cert.publicKey);
-let pemPrivateKey = pki.privateKeyToPem(keys.privateKey);
+if (!fs.existsSync("./Certificates/rootCertificate/privateKey.pem")) {
+  console.log("No existe ./Certificates/rootCertificate/privateKey.pem");
+  rootCertificateCertAndKeyPairExist = false;
+} else {
+  const rootPrivateKeyPEM = fs.readFileSync(
+    "./Certificates/rootCertificate/privateKey.pem",
+    "utf-8"
+  );
 
-cert.serialNumber = "02";
-cert.validity.notBefore = new Date();
-cert.validity.notAfter = new Date();
-cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+  console.log("rootPrivateKeyPEM: \n", rootPrivateKeyPEM);
+  rootPrivateKey = pki.privateKeyFromPem(rootPrivateKeyPEM);
+}
 
-var attrs = [
-  {
-    name: "commonName",
-    value: "Child Certificate",
-  },
-  {
-    name: "countryName",
-    value: "GT",
-  },
-  {
-    shortName: "ST",
-    value: "Guatemala City",
-  },
-  {
-    name: "localityName",
-    value: "Guatemala",
-  },
-  {
-    name: "organizationName",
-    value: "Tribunal Supremo Electoral",
-  },
-  {
-    shortName: "OU",
-    value: "Tribunal Supremo Electoral",
-  },
-];
+if (rootCertificateCertAndKeyPairExist) {
+  // generate a keypair or use one you have already
+  var keys = pki.rsa.generateKeyPair(2048);
 
-const rootCertificatePEM = fs.readFileSync(
-  "./Certificates/rootCertificate/rootCertificate.pem",
-  "utf-8"
-);
-console.log("rootCertificatePEM: \n", rootCertificatePEM, "\n");
+  // create a new certificate
+  var cert = pki.createCertificate();
 
-let rootCertificate = pki.certificateFromPem(rootCertificatePEM);
+  // fill the required fields
+  cert.publicKey = keys.publicKey;
+  let pemPublicKey = pki.publicKeyToPem(cert.publicKey);
+  let pemPrivateKey = pki.privateKeyToPem(keys.privateKey);
 
-const rootPrivateKeyPEM = fs.readFileSync(
-  "./Certificates/rootCertificate/privateKey.pem",
-  "utf-8"
-);
+  let serialNumber = "0001";
+  let certificateRoute = tempDir + "/" + serialNumber;
+  // console.log("certificateRoute:", certificateRoute);
 
-console.log("rootPrivateKeyPEM: \n", rootPrivateKeyPEM);
-let rootPrivateKey = pki.privateKeyFromPem(rootPrivateKeyPEM);
+  let serialNumberDetermined = false;
 
-cert.setSubject(attrs);
+  while (serialNumberDetermined === false) {
+    if (!fs.existsSync(certificateRoute)) {
+      //fs.mkdirSync(certificateRoute);
+      serialNumberDetermined = true;
+    } else {
+      serialNumber = Number(serialNumber) + 1;
+      serialNumber = serialNumber.toString();
+      switch (serialNumber.length) {
+        case 1:
+          serialNumber = "000" + serialNumber;
+          console.log("new serialNumber:", serialNumber);
+          break;
+        case 2:
+          serialNumber = "00" + serialNumber;
+          console.log("new serialNumber:", serialNumber);
+          break;
+        case 3:
+          serialNumber = "0" + serialNumber;
+          console.log("new serialNumber:", serialNumber);
+          break;
+        case 4:
+          console.log("new serialNumber:", serialNumber);
+          break;
 
-/* console.log(
+        default:
+        // code block
+      }
+      certificateRoute = tempDir + "/" + serialNumber;
+    }
+  }
+
+  cert.serialNumber = serialNumber;
+  cert.validity.notBefore = new Date();
+  cert.validity.notAfter = new Date();
+  cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+
+  var attrs = [
+    {
+      name: "commonName",
+      value: "Child Certificate",
+    },
+    {
+      name: "countryName",
+      value: "GT",
+    },
+    {
+      shortName: "ST",
+      value: "Guatemala City",
+    },
+    {
+      name: "localityName",
+      value: "Guatemala",
+    },
+    {
+      name: "organizationName",
+      value: "Tribunal Supremo Electoral",
+    },
+    {
+      shortName: "OU",
+      value: "Tribunal Supremo Electoral",
+    },
+  ];
+
+  cert.setSubject(attrs);
+
+  /* console.log(
   "rootCertificate.subject.attributes:",
   rootCertificate.subject.attributes
 ); */
 
-cert.setIssuer(rootCertificate.subject.attributes);
-//cert.setIssuer(attrs);
+  cert.setIssuer(rootCertificate.subject.attributes);
+  //cert.setIssuer(attrs);
 
-// the actual certificate signing
-cert.sign(rootPrivateKey);
+  // the actual certificate signing
+  cert.sign(rootPrivateKey);
 
-// now convert the Forge certificate to PEM format
-var pemCertificate = pki.certificateToPem(cert);
+  // now convert the Forge certificate to PEM format
+  var pemCertificate = pki.certificateToPem(cert);
 
-console.log("Certificate on pem format: \n", pemCertificate);
-console.log("pemPrivateKey: \n", pemPrivateKey);
-console.log("pemPublicKey: \n", pemPublicKey);
+  //console.log("Certificate on pem format: \n", pemCertificate);
+  //console.log("pemPrivateKey: \n", pemPrivateKey);
+  //console.log("pemPublicKey: \n", pemPublicKey);
 
-let verified;
-try {
-  //Cambiar cert por rootCertificate y viceversa para probar el rechazo de una validación.
-  verified = rootCertificate.verify(cert, function (err) {
-    if (err) return err;
-  });
-} catch {
-  console.log("No ha podido ser verificado el certificado.");
-  verified = false;
-}
-
-console.log(
-  "Certificado root ha verificado con su propia llave pública al certificado hijo:",
-  verified
-);
-
-if (verified) {
-  let tempDir = "./Certificates";
-  if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir);
+  let verified;
+  try {
+    //Cambiar cert por rootCertificate y viceversa para probar el rechazo de una validación.
+    verified = rootCertificate.verify(cert, function (err) {
+      if (err) return err;
+    });
+  } catch {
+    console.log("No ha podido ser verificado el certificado.");
+    verified = false;
   }
 
-  let tempDir2 = "./Certificates/01/";
+  console.log(
+    "Certificado root ha verificado con su propia llave pública al certificado hijo:",
+    verified
+  );
 
-  if (!fs.existsSync(tempDir2)) {
-    fs.mkdirSync(tempDir2);
+  if (verified) {
+    let tempDir = "./Certificates";
+    if (!fs.existsSync(tempDir)) {
+      fs.mkdirSync(tempDir);
+    }
+
+    if (!fs.existsSync(certificateRoute)) {
+      fs.mkdirSync(certificateRoute);
+    }
+
+    fs.writeFile(certificateRoute + "/cert.pem", pemCertificate, function (
+      err
+    ) {
+      if (err) throw err;
+      console.log(
+        "cert.pem succesfully done writing on " +
+          certificateRoute +
+          "/cert.pem."
+      );
+    });
+
+    fs.writeFile(certificateRoute + "/privateKey.pem", pemPrivateKey, function (
+      err
+    ) {
+      if (err) throw err;
+      console.log(
+        "privateKey.pem succesfully done writing on " +
+          certificateRoute +
+          "/privateKey.pem."
+      );
+    });
+
+    fs.writeFile(certificateRoute + "/publicKey.pem", pemPublicKey, function (
+      err
+    ) {
+      if (err) throw err;
+      console.log(
+        "publicKey.pem succesfully done writing on " +
+          certificateRoute +
+          "/publicKey.pem."
+      );
+    });
+  } else {
+    console.log(
+      "No se ha escrito el certificado ni sus llaves en el directorio porque" +
+        " no pudo ser verificado con el Root Certificate."
+    );
   }
-
-  fs.writeFile("./Certificates/01/cert.pem", pemCertificate, function (err) {
-    if (err) throw err;
-    console.log("cert.pem succesfully done writing.");
-  });
-
-  fs.writeFile("./Certificates/01/privateKey.pem", pemPrivateKey, function (
-    err
-  ) {
-    if (err) throw err;
-    console.log("privateKey.pem succesfully done writing.");
-  });
-
-  fs.writeFile("./Certificates/01/publicKey.pem", pemPublicKey, function (err) {
-    if (err) throw err;
-    console.log("publicKey.pem succesfully done writing.");
-  });
 } else {
   console.log(
-    "No se ha escrito el certificado ni sus llaves en el directorio porque" +
-      " no pudo ser verificado con el Root Certificate."
+    "No se pudo crear el nuevo certificado debido a que no se ha ubicado un certificado root."
   );
 }
